6f03cb4bc58dac437570df152b7e1fb80cb23327
SumonaYeasmin <sumonayeasmin570@gmail.com>
2025-04-25 12:32:42 +0600

    update Errorpage functionality with Go back button

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,27 @@
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+    const navigate = useNavigate(); 
     return (
         <div className="flex flex-col items-center justify-center h-screen text-center">
             <Helmet>
                 <title>Error | FindMate</title>
             </Helmet>
             <img className="h-2/4" src="https://i.ibb.co.com/sv2WWXKW/404-Error-rafiki-removebg-preview.png" alt="" />
-            <Link to="/" className="text-xl -mt-10 text-indigo-600 font-bold border border-indigo-500 rounded-full px-4 py-1 hover:bg-indigo-300 transition duration-300  bg-white ">
-                🔙 Go Back Home
-            </Link>
+            <div className="space-x-2">
+                <button
+                    onClick={() => navigate(-1)}
+                    className="text-xl -mt-10 text-indigo-600 font-bold border border-indigo-500 rounded-full px-4 py-1 hover:bg-indigo-300 transition duration-300  bg-white "
+                >
+                    Go Back
+                </button>
+                <Link to="/" className="text-xl -mt-10 text-indigo-600 font-bold border border-indigo-500 rounded-full px-4 py-1 hover:bg-indigo-300 transition duration-300  bg-white ">
+                    🔙 Go Back Home
+                </Link>
+
+            </div>
+
         </div>
     );
 };
